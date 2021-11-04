4f03408314cddb0587486fe43afc9f126dfc9df5
Chris Scott <christocracy@gmail.com>
2021-11-03 23:58:20 -0400

    Add new React-based demo app.  /example folder now hosts two apps (angular, react)

---
+++ example/react/src/App.tsx
@@ -0,0 +1,77 @@
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Storage } from '@capacitor/storage';
+import React from "react";
+
+import { useIonAlert, useIonToast } from '@ionic/react';
+
+import Home from './pages/home/Home';
+import HelloWorld from './pages/hello-world/HelloWorld';
+import AdvancedApp from './pages/advanced/AdvancedApp';
+import SettingsService from "./pages/advanced/lib/SettingsService";
+
+import "./App.css"
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const App: React.FC = () => {
+  /// Provide some UI assets to SettingsService so it can use its methods.
+  /// WARNING:  I found I had to disable animations due to timing issues showing
+  /// alerts simultaneously, where the second would get hidden from the first's
+  /// final teardown after hide animation some between 10-99ms laster.
+  ///
+  /// - prompt()
+  /// - yesNo()
+  /// - alert()
+  /// - confirm()
+  /// - toast()
+  const [alert] = useIonAlert();
+  const [toast] = useIonToast();
+
+  SettingsService.getInstance().configure({
+    alert: alert,
+    toast: toast
+  });
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/hello-world">
+            <HelloWorld />
+          </Route>
+          <Route exact path="/advanced">
+            <AdvancedApp />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
+
+export default App;
---
+++ example/react/src/pages/advanced/GeofenceView.tsx
@@ -0,0 +1,130 @@
+/// View for adding a custom Geofence.
+/// This view is activated by long-pressing on the Map.
+///
+import {
+  IonHeader,
+  IonContent,
+  IonIcon,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonSelect,
+  IonSelectOption,
+  IonItem,
+  IonInput,
+  IonToggle,
+  IonLabel,
+} from '@ionic/react';
+
+import React from "react";
+
+import { add } from "ionicons/icons";
+
+import BackgroundGeolocation from "@transistorsoft/capacitor-background-geolocation";
+import SettingsService from "./lib/SettingsService";
+
+const validate = (value:string) => {
+  return (value !== null) && (value.length > 0);
+}
+
+const GeofenceView: React.FC = (props:any) => {
+  const settingsService = SettingsService.getInstance();
+
+  /// State
+  const [identifier, setIdentifier] = React.useState('');
+  const [radius, setRadius] = React.useState(200);
+  const [notifyOnEntry, setNotifyOnEntry] = React.useState(true);
+  const [notifyOnExit, setNotifyOnExit] = React.useState(true);
+  const [notifyOnDwell, setNotifyOnDwell] = React.useState(false);
+  const [loiteringDelay, setLoiteringDelay] = React.useState('10000');
+
+  /// [Add Geofence] button-handler.
+  const onClickAdd = () => {
+    if (!validate(identifier)) {
+      settingsService.toast('Identifier is required');
+      return;
+    } else if (!notifyOnEntry && !notifyOnExit && !notifyOnDwell) {
+      settingsService.toast('You must select at least one geofence transition');
+      return;
+    }
+
+    BackgroundGeolocation.addGeofence({
+      identifier: identifier,
+      latitude: props.coordinate.lat(),
+      longitude: props.coordinate.lng(),
+      radius: radius,
+      notifyOnEntry: notifyOnEntry,
+      notifyOnExit: notifyOnExit,
+      notifyOnDwell: notifyOnDwell,
+      loiteringDelay: parseInt(loiteringDelay, 10)
+    }).then((result) => {
+      settingsService.playSound('ADD_GEOFENCE');
+      props.onDismiss();
+    }).catch((error) => {
+      settingsService.alert('Add Geofence Error', error);
+    })
+  }
+
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar color="tertiary">
+          <IonButtons slot="start">
+            <IonButton onClick={props.onDismiss}><IonIcon name="close" /></IonButton>
+          </IonButtons>
+          <IonTitle>Add Geofence</IonTitle>
+
+          <IonButtons slot="end">
+            <IonButton color="primary" fill="solid" size="large" onClick={onClickAdd}>
+            <IonIcon icon={add} />Add Geofence
+          </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent>
+        <IonItem>
+          <IonLabel position="stacked" color="primary">Identifier:</IonLabel>
+          <IonInput value={identifier} placeholder="Unique geofence identifier" onIonChange={(e:any) => setIdentifier(e.detail.value)}/>
+        </IonItem>
+
+        <IonItem>
+          <IonLabel position="stacked" color="primary">Radius:</IonLabel>
+          <IonSelect value={radius} onIonChange={(e) => setRadius(e.detail.value)}>
+            <IonSelectOption value={150}>150</IonSelectOption>
+            <IonSelectOption value={200}>200</IonSelectOption>
+            <IonSelectOption value={500}>500</IonSelectOption>
+            <IonSelectOption value={1000}>1000</IonSelectOption>
+            <IonSelectOption value={2000}>2000</IonSelectOption>
+            <IonSelectOption value={5000}>5000</IonSelectOption>
+            <IonSelectOption value={10000}>10000</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+
+        <IonItem>
+          <IonLabel color="primary">notifyOnEntry:</IonLabel>
+          <IonToggle checked={notifyOnEntry} onIonChange={(e:any) => setNotifyOnEntry(e.detail.checked)} />
+        </IonItem>
+
+        <IonItem>
+          <IonLabel color="primary">notifyOnExit:</IonLabel>
+          <IonToggle checked={notifyOnExit} onIonChange={(e:any) => setNotifyOnExit(e.detail.checked)} />
+        </IonItem>
+
+        <IonItem>
+          <IonLabel color="primary">notifyOnDwell:</IonLabel>
+          <IonToggle checked={notifyOnDwell} onIonChange={(e:any) => setNotifyOnDwell(e.detail.checked)} />
+        </IonItem>
+
+        <IonItem>
+          <IonLabel position="stacked" color="primary">loiteringDelay:</IonLabel>
+          <IonInput value={loiteringDelay} placeholder="Time in milliseconds before notifyOnDwell transition fires." onIonChange={(e:any) => setIdentifier(e.detail.value)}/>
+        </IonItem>
+
+      </IonContent>
+    </>
+  )
+};
+
+export default GeofenceView;
