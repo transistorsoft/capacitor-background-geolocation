6427ea69d5688ec2b8213619f2c6709a5e71bf75
Chris Scott <chris@transistorsoft.com>
2023-09-22 16:45:14 -0400

    Only zoom map onMotionChange

--- example/react/src/pages/advanced/lib/GeoMath.ts
+++ example/react/src/pages/advanced/lib/GeoMath.ts
@@ -1,6 +1,7 @@
-///
 /// Map geometry methods for calculating Geofence hit-markers on MapView
-//
+/// @author Chris Scott, Transistor Software
+///
+
 export const toRad = (n) => {
   return n * (Math.PI / 180);
 }
@@ -8,20 +9,20 @@ export const toDeg = (n) => {
   return n * (180 / Math.PI);
 }
 
-export const getBearing = (start:any, end:any) => {
-  let startLat = toRad(start.latitude);
-  let startLong = toRad(start.longitude);
-  let endLat = toRad(end.latitude);
-  let endLong = toRad(end.longitude);
+/// Calculate bearing based upon the vector of two locations
+/// @param origin {latitude, longitude}
+/// @param destination {latitude, longitude}
+/// @return number Bearing in compass degrees (0-360)...or is it 0-359?
+export const getBearing = (origin:any, destination:any) => {
+  const originLat       = toRad(origin.latitude);
+  const originLong      = toRad(origin.longitude);
+  const destinationLat  = toRad(destination.latitude);
+  const destinationLong = toRad(destination.longitude);
 
-  let dLong = endLong - startLong;
+  let dLong = destinationLong - originLong;
 
-  let dPhi = Math.log(
-  	Math.tan(
-  		endLat / 2.0 + Math.PI / 4.0
-  	) / Math.tan(
-  		startLat / 2.0 + Math.PI / 4.0
-  	)
+  const dPhi = Math.log(
+    Math.tan(destinationLat / 2.0 + Math.PI / 4.0) / Math.tan(originLat / 2.0 + Math.PI / 4.0)
   );
 
   if (Math.abs(dLong) > Math.PI){
@@ -33,17 +34,19 @@ export const getBearing = (start:any, end:any) => {
   return (toDeg(Math.atan2(dLong, dPhi)) + 360.0) % 360.0;
 }
 
+/// Calculates a new {latitude, longitude} some distance away in space in some direction.
+///
 export const computeOffsetCoordinate = (coordinate:any, distance:number, heading:number) => {
   distance = distance / (6371*1000);
   heading = toRad(heading);
 
-  let lat1 = toRad(coordinate.latitude);
-  let lon1 = toRad(coordinate.longitude);
-  let lat2 = Math.asin(
-  	Math.sin(lat1) * Math.cos(distance) + Math.cos(lat1) * Math.sin(distance) * Math.cos(heading)
+  const lat1 = toRad(coordinate.latitude);
+  const lon1 = toRad(coordinate.longitude);
+  const lat2 = Math.asin(
+    Math.sin(lat1) * Math.cos(distance) + Math.cos(lat1) * Math.sin(distance) * Math.cos(heading)
   );
-  let lon2 = lon1 + Math.atan2(
-  	Math.sin(heading) * Math.sin(distance) * Math.cos(lat1),
+  const lon2 = lon1 + Math.atan2(
+    Math.sin(heading) * Math.sin(distance) * Math.cos(lat1),
     Math.cos(distance) - Math.sin(lat1) * Math.sin(lat2)
   );
 
